be2690aa28e4122e71c50e37d536bd366e54d2bd
idonotknowwhatiamdoing <zhongshera@gmail.com>
2019-06-06 08:51:49 -0700

    committed

---
+++ apcsp-final/hangmanimage.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(void)
+{
+
+int mistakes = 4; 
+	{
+	if (mistakes == 0) 
+		{
+		printf("________\n");
+		printf("|	\n");
+		printf("|	\n");
+		printf("|	\n");
+		printf("|	\n");
+		printf("|	\n");
+		printf("________\n");
+		}
+	else if (mistakes == 1) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("________\n");
+                }
+	else if (mistakes == 2) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|     ⚇ \n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("________\n");
+		}
+	else if (mistakes == 3) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|     ⚇ \n");
+                printf("|     | \n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("________\n");
+		}
+	else if (mistakes == 4) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|     ⚇ \n");
+                printf("|    /|	\n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("________\n");
+		}
+	else if (mistakes == 5) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|     ⚇ \n");
+                printf("|    /|\\n");
+                printf("|       \n");
+                printf("|       \n");
+                printf("________\n");
+		}
+	else if (mistakes == 6) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|     ⚇ \n");
+                printf("|    /|\\n");
+                printf("|    /  \n");
+                printf("|       \n");
+                printf("________\n");
+		}
+	else if (mistakes == 7) 
+                {
+                printf("________\n");
+                printf("|     | \n");
+                printf("|     ⚇ \n");
+                printf("|    /|\ \n");
+                printf("|    / \ \n");
+                printf("|       \n");
+                printf("________\n");
+		}
+	}
+}
